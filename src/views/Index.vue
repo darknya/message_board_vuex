88be3ab2e30d24a542be235c1716b5317c8d1829
darknya <dark801109@gmail.com>
2020-05-04 16:03:49 +0800

    add all

---
+++ src/views/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+import Footer from '../components/Footer.vue';
+
+export default {
+  name: 'Index',
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
